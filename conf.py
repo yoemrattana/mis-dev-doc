167b676fa3fc4fc610f59c3d49a5622539d8ffca
@rattana <yoemrattana@gmail.com>
2022-01-03 12:06:02 +0700

    u

--- conf.py
+++ conf.py
@@ -68,8 +68,10 @@ html_theme = 'sphinx_rtd_theme'
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
 
-latex_engine = 'xelatex'
-latex_use_xindy = False
+
+
 latex_elements = {
   'extraclassoptions': 'openany,oneside'
 }
+
+latex_engine = 'xelatex'
